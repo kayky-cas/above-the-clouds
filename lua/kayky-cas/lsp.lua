c908d5a724f312f5556ac7e77a1f584a49496291
Kayky Casagrande <kayky-cas@github.com>
2022-11-20 01:05:06 -0300

    lua lsp server

--- lua/kayky-cas/lsp.lua
+++ lua/kayky-cas/lsp.lua
@@ -10,3 +10,25 @@ nvim_lsp.tsserver.setup {
   root_dir = nvim_lsp.util.root_pattern("package.json"),
 }
 
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
--- lua/kayky-cas/packer.lua
+++ lua/kayky-cas/packer.lua
@@ -1,38 +1,34 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'folke/tokyonight.nvim'
+    use("wbthomason/packer.nvim")
+
     use("neovim/nvim-lspconfig")
-    -- Visualize lsp progress
+
     use({
-      "j-hui/fidget.nvim",
-      config = function()
-        require("fidget").setup()
-      end
+        "j-hui/fidget.nvim",
+        config = function()
+            require("fidget").setup()
+        end
     })
 
-    -- Autocompletion framework
     use("hrsh7th/nvim-cmp")
+
     use({
-      -- cmp LSP completion
-      "hrsh7th/cmp-nvim-lsp",
-      -- cmp Snippet completion
-      "hrsh7th/cmp-vsnip",
-      -- cmp Path completion
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      after = { "hrsh7th/nvim-cmp" },
-      requires = { "hrsh7th/nvim-cmp" },
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        after = { "hrsh7th/nvim-cmp" },
+        requires = { "hrsh7th/nvim-cmp" },
     })
-    -- See hrsh7th other plugins for more great completion sources!
-    -- Snippet engine
-    use('hrsh7th/vim-vsnip')
-    -- Adds extra functionality over rust analyzer
+
+    use("hrsh7th/vim-vsnip")
+
     use("simrat39/rust-tools.nvim")
 
     use("nvim-lua/popup.nvim")
+
     use("nvim-lua/plenary.nvim")
 
     -- Auto close {}, (), "", etc..
@@ -40,35 +36,28 @@ return require('packer').startup(function(use)
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
-    
+
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
-    -- using packer.nvim
-    -- use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-
-    -- Lua
     use {
         "folke/zen-mode.nvim",
         config = function()
             require("zen-mode").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
             }
         end
     }
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use { "mfussenegger/nvim-jdtls", ft = { "java" } }
 
-    use ('Mofiqul/dracula.nvim')
+    use('Mofiqul/dracula.nvim')
 
     use {
         'nvim-treesitter/nvim-treesitter',
@@ -80,5 +69,5 @@ return require('packer').startup(function(use)
         config = function() require("tmux").setup() end
     })
 
-    use ('kyazdani42/nvim-web-devicons')
+    use('kyazdani42/nvim-web-devicons')
 end)
