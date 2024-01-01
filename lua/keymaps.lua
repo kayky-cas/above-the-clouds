b6ef5a661bda9931cc370d97adfbf7dadc111195
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2023-12-31 23:16:05 -0300

    its going

---
+++ lua/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", {})
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", {})
+vim.keymap.set("n", "<leader>Q", "<cmd>q!<cr>", {})
---
+++ lua/plugins.lua
@@ -0,0 +1,8 @@
+return {
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+    { "stevearc/dressing.nvim" },
+}
+
